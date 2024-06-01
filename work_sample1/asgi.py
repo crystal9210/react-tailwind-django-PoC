c8020832322709e8e2733ef4cbaea173e508cbcf
crystal9210 <sy9210sss@yahoo.co.jp>
2024-06-01 23:54:33 +0900

    Add django-environ to requirements.txt

--- work_sample1/asgi.py
+++ work_sample1/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for mysite project.
+ASGI config for work_sample1 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'work_sample1.settings')
 
 application = get_asgi_application()
