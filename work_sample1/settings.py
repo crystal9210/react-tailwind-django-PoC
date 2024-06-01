333dd2d07c497bde970f9452bbc2e7118b3e84f0
crystal9210 <sy9210sss@yahoo.co.jp>
2024-06-02 04:41:54 +0900

    challenge

--- work_sample1/settings.py
+++ work_sample1/settings.py
@@ -122,9 +122,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "frontend/static"),
-]
+# デプロイ環境かローカル環境かで設定を分ける
+if os.getenv('RENDER', 'False') == 'True':
+    # デプロイ環境 (Render)
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "static"),
+    ]
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+else:
+    # ローカル環境
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "static"),
+        os.path.join(BASE_DIR, "frontend/static"),
+    ]
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Renderのビルド時に静的ファイルを収集するディレクトリを指定しないとエラーが出力されるので設定
