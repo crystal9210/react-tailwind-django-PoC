f97972df7f1541466775b9c55deab321d7af242a
crystal9210 <sy9210sss@yahoo.co.jp>
2024-06-02 05:01:06 +0900

    change base.html import urls

--- work_sample1/settings.py
+++ work_sample1/settings.py
@@ -121,19 +121,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'frontend/static/'
 # デプロイ環境かローカル環境かで設定を分ける
 if os.getenv('RENDER', 'False') == 'True':
     # デプロイ環境 (Render)
     STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, "static"),
+        os.path.join(BASE_DIR, "frontend/static"),
     ]
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 else:
     # ローカル環境
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, "static"),
-        os.path.join(BASE_DIR, "frontend/static"),
+        os.path.join(BASE_DIR, "static"),
     ]
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
